0122052b590640a6fe2077e0d1193244e53ab5bb
Max Kardanets <max.kardanets@gmail.com>
2021-04-20 20:05:44 +0300

    ADDED | Main theme files

---
+++ wp-content/themes/apple/404.php
@@ -0,0 +1,19 @@
+<?php
+get_header();
+
+$page_title = __('404 Not Found');
+?>
+<nav class="breadcrumb-section theme1 bg-lighten2 pt-110 pb-110">
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<div class="section-title text-center mb-15">
+					<h2 class="title text-dark text-capitalize"><?php echo $page_title; ?></h2>
+				</div>
+			</div>
+		</div>
+	</div>
+</nav>
+<?php
+get_footer();
+?>
---
+++ wp-content/themes/apple/archive.php
@@ -0,0 +1,232 @@
+<?php
+get_header();
+?>
+<!-- breadcrumb-section start -->
+<nav class="breadcrumb-section theme1 bg-lighten2 pt-110 pb-110">
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<div class="section-title text-center mb-15">
+					<h2 class="title text-dark text-capitalize">blog list left sidebar</h2>
+				</div>
+			</div>
+			<div class="col-12">
+				<ol class="breadcrumb bg-transparent m-0 p-0 align-items-center justify-content-center">
+					<li class="breadcrumb-item"><a href="index.html">Home</a></li>
+					<li class="breadcrumb-item active" aria-current="page">blog list left sidebar</li>
+				</ol>
+			</div>
+		</div>
+	</div>
+</nav>
+<!-- breadcrumb-section end -->
+<!-- product tab start -->
+<section class="blog-section pt-80 pb-50">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-9">
+				<div class="row">
+					<div class="col-12 mb-30">
+						<div class="single-blog media flex-column flex-md-row">
+							<a class="blog-thumb mb-20 mb-md-0 mr-md-4 mr-xl-5 w-md-50 zoom-in d-block overflow-hidden"
+							   href="blog-grid-left-sidebar.html">
+								<img src="<?php echo $temp_html_dir; ?>assets/img/blog-post/1.jpg" alt="blog-thumb-naile">
+							</a>
+							<div class="blog-post-content media-body mb-5 mb-md-0">
+								<h3 class="title mb-15"><a href="single-blog.html">This is first Post For XipBlog</a>
+								</h3>
+								<h5 class="sub-title"> Posted by <a class="blog-link"
+																	href="https://themeforest.net/user/hastech">HasTech</a> <span
+										class="separator">/</span> 27 sep, 2020</h5>
+								<p class="text">
+									Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+									Ipsum has been the industrys standard dummy text ever since the 1500s, when ...
+								</p>
+								<a class="read-more" href="single-blog.html">Read More</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-12 mb-30">
+						<div class="single-blog media flex-column flex-md-row">
+							<a class="blog-thumb mb-20 mb-md-0 mr-md-4 mr-xl-5 w-md-50 zoom-in d-block overflow-hidden"
+							   href="blog-grid-left-sidebar.html">
+								<img src="<?php echo $temp_html_dir; ?>assets/img/blog-post/2.jpg" alt="blog-thumb-naile">
+							</a>
+							<div class="blog-post-content media-body mb-5 mb-md-0">
+								<h3 class="title mb-15"><a href="single-blog.html">This is first Post For XipBlog</a>
+								</h3>
+								<h5 class="sub-title"> Posted by <a class="blog-link"
+																	href="https://themeforest.net/user/hastech">HasTech</a> <span
+										class="separator">/</span> 27 sep, 2020 </h5>
+								<p class="text">
+									Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+									Ipsum has been the industrys standard dummy text ever since the 1500s, when ...
+								</p>
+								<a class="read-more" href="single-blog.html">Read More</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-12 mb-30">
+						<div class="single-blog media flex-column flex-md-row">
+							<a class="blog-thumb mb-20 mb-md-0 mr-md-4 mr-xl-5 w-md-50 zoom-in d-block overflow-hidden"
+							   href="blog-grid-left-sidebar.html">
+								<img src="<?php echo $temp_html_dir; ?>assets/img/blog-post/3.jpg" alt="blog-thumb-naile">
+							</a>
+							<div class="blog-post-content media-body mb-5 mb-md-0">
+								<h3 class="title mb-15"><a href="single-blog.html">This is first Post For XipBlog</a>
+								</h3>
+								<h5 class="sub-title"> Posted by <a class="blog-link"
+																	href="https://themeforest.net/user/hastech">HasTech</a> <span
+										class="separator">/</span> 27 sep, 2020</h5>
+								<p class="text">
+									Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+									Ipsum has been the industrys standard dummy text ever since the 1500s, when ...
+								</p>
+								<a class="read-more" href="single-blog.html">Read More</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-12 mb-30">
+						<div class="single-blog media flex-column flex-md-row">
+							<a class="blog-thumb mb-20 mb-md-0 mr-md-4 mr-xl-5 w-md-50 zoom-in d-block overflow-hidden"
+							   href="blog-grid-left-sidebar.html">
+								<img src="<?php echo $temp_html_dir; ?>assets/img/blog-post/2.jpg" alt="blog-thumb-naile">
+							</a>
+							<div class="blog-post-content media-body mb-5 mb-md-0">
+								<h3 class="title mb-15"><a href="single-blog.html">This is first Post For XipBlog</a>
+								</h3>
+								<h5 class="sub-title"> Posted by <a class="blog-link"
+																	href="https://themeforest.net/user/hastech">HasTech</a> <span
+										class="separator">/</span> 27 sep, 2020</h5>
+								<p class="text">
+									Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+									Ipsum has been the industrys standard dummy text ever since the 1500s, when ...
+								</p>
+								<a class="read-more" href="single-blog.html">Read More</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-12 mb-30">
+						<div class="single-blog media flex-column flex-md-row">
+							<a class="blog-thumb mb-20 mb-md-0 mr-md-4 mr-xl-5 w-md-50 zoom-in d-block overflow-hidden"
+							   href="blog-grid-left-sidebar.html">
+								<img src="<?php echo $temp_html_dir; ?>assets/img/blog-post/3.jpg" alt="blog-thumb-naile">
+							</a>
+							<div class="blog-post-content media-body mb-5 mb-md-0">
+								<h3 class="title mb-15"><a href="single-blog.html">This is first Post For XipBlog</a>
+								</h3>
+								<h5 class="sub-title"> Posted by <a class="blog-link"
+																	href="https://themeforest.net/user/hastech">HasTech</a> <span
+										class="separator">/</span> 27 sep, 2020</h5>
+								<p class="text">
+									Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+									Ipsum has been the industrys standard dummy text ever since the 1500s, when ...
+								</p>
+								<a class="read-more" href="single-blog.html">Read More</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-12 mb-30">
+						<div class="single-blog media flex-column flex-md-row">
+							<a class="blog-thumb mb-20 mb-md-0 mr-md-4 mr-xl-5 w-md-50 zoom-in d-block overflow-hidden"
+							   href="blog-grid-left-sidebar.html">
+								<img src="<?php echo $temp_html_dir; ?>assets/img/blog-post/1.jpg" alt="blog-thumb-naile">
+							</a>
+							<div class="blog-post-content media-body mb-5 mb-md-0">
+								<h3 class="title mb-15"><a href="single-blog.html">This is first Post For XipBlog</a>
+								</h3>
+								<h5 class="sub-title"> Posted by <a class="blog-link"
+																	href="https://themeforest.net/user/hastech">HasTech</a> <span
+										class="separator">/</span> 27 sep, 2020</h5>
+								<p class="text">
+									Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+									Ipsum has been the industrys standard dummy text ever since the 1500s, when ...
+								</p>
+								<a class="read-more" href="single-blog.html">Read More</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-3 order-lg-first mb-30">
+				<aside class="blog-left-sidebar">
+					<div class="sidebar-widget theme1 mb-30">
+						<h3 class="post-title">Search</h3>
+						<div class="blog-search-form">
+							<form action="#" class="position-relative">
+								<input class="form-control border-blue rounded" type="text"
+									   placeholder="Enter your search key ...">
+								<button class="btn blog-search-btn" type="submit"><i
+										class="ion-ios-search-strong"></i></button>
+							</form>
+						</div>
+					</div>
+					<div class="sidebar-widget mb-30">
+						<h3 class="post-title">Categories</h3>
+						<ul class="blog-links">
+							<li><a href="#">Dresses (20)</a></li>
+							<li><a href="#">Jackets &amp; Coats (9)</a></li>
+							<li><a href="#">Sweaters (5)</a></li>
+							<li><a href="#">Jeans (11)</a></li>
+							<li><a href="#">Blouses &amp; Shirts (3)</a></li>
+							<li><a href="#">Electronic Cigarettes (6)</a></li>
+							<li><a href="#">Bags &amp; Cases (4)</a></li>
+						</ul>
+					</div>
+					<div class="sidebar-widget mb-30">
+						<h3 class="post-title">Recent Post</h3>
+						<div class="blog-media-list mb-30 media">
+							<div class="post-thumb mr-4">
+								<a href="#"><img src="<?php echo $temp_html_dir; ?>assets/img/blog-post/1.jpg" alt="img"></a>
+							</div>
+							<div class="media-body">
+								<h5 class="sub-title mb-1"><a href="single-blog.html">This Is First Post </a></h5>
+								<span class="date">Lorem Ipsum is simply</span>
+							</div>
+						</div>
+						<div class="blog-media-list mb-30 media">
+							<div class="post-thumb mr-4">
+								<a href="#"><img src="<?php echo $temp_html_dir; ?>assets/img/blog-post/2.jpg" alt="img"></a>
+							</div>
+							<div class="media-body">
+								<h5 class="sub-title mb-1"><a href="single-blog.html">This Is Secound Post </a></h5>
+								<span class="date">Lorem Ipsum is simply</span>
+							</div>
+						</div>
+						<div class="blog-media-list mb-30 media">
+							<div class="post-thumb mr-4">
+								<a href="#"><img src="<?php echo $temp_html_dir; ?>assets/img/blog-post/3.jpg" alt="img"></a>
+							</div>
+							<div class="media-body">
+								<h5 class="sub-title mb-1"><a href="single-blog.html">This Is Third Post </a></h5>
+								<span class="date">Lorem Ipsum is simply</span>
+							</div>
+						</div>
+						<div class="blog-media-list media">
+							<div class="post-thumb mr-4">
+								<a href="#"><img src="<?php echo $temp_html_dir; ?>assets/img/blog-post/1.jpg" alt="img"></a>
+							</div>
+							<div class="media-body">
+								<h5 class="sub-title mb-1"><a href="single-blog.html">This Is Fourth Post </a></h5>
+								<span class="date">Lorem Ipsum is simply</span>
+							</div>
+						</div>
+					</div>
+					<div class="sidebar-widget">
+						<h3 class="post-title">Tags</h3>
+						<ul class="product-tag d-flex flex-wrap">
+							<li><a href="#">Computer</a></li>
+							<li><a href="#">sale</a></li>
+							<li><a href="#">Electronics</a></li>
+							<li><a href="#">Jewelry &amp; Watches</a></li>
+						</ul>
+					</div>
+				</aside>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- product tab end -->
+<?php
+get_footer();
+?>
