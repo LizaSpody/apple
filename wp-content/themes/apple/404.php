bdee8e2dc7e00595b88979b0e095917554d616fa
Max Kardanets <max.kardanets@gmail.com>
2021-04-24 00:09:04 +0300

    UPDATED | 404 page | Title block (HTML -> function)

--- wp-content/themes/apple/404.php
+++ wp-content/themes/apple/404.php
@@ -2,18 +2,9 @@
 get_header();
 
 $page_title = __('404 Not Found');
+
+echo get_theme_page_title_block($page_title, false);
 ?>
-<nav class="breadcrumb-section theme1 bg-lighten2 pt-110 pb-110">
-	<div class="container">
-		<div class="row">
-			<div class="col-12">
-				<div class="section-title text-center mb-15">
-					<h2 class="title text-dark text-capitalize"><?php echo $page_title; ?></h2>
-				</div>
-			</div>
-		</div>
-	</div>
-</nav>
 <?php
 get_footer();
 ?>
