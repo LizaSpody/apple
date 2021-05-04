7f211ccd5f53c72e88c94db7b83caf5582b3761c
Max Kardanets <max.kardanets@gmail.com>
2021-05-04 16:14:56 +0300

    UPDATED | Single news page

--- wp-content/themes/apple/single.php
+++ wp-content/themes/apple/single.php
@@ -1,34 +1,30 @@
 <?php
-$title = get_the_title();
+the_post();
+
+$post_title = get_the_title();
 $html_thumbnail = get_the_post_thumbnail();
-$date = get_the_date();
-$content = get_the_content();
+$post_date = get_the_date();
+$post_categories = get_the_category_list();
+$post_author = get_the_author_posts_link();
+$post_content = get_the_content();
 
 get_header();
 
-echo get_theme_page_title_block($title);
+echo get_theme_page_title_block($post_title);
 ?>
 <!-- product tab start -->
 <section class="blog-section pt-80 pb-80">
 	<div class="container">
 		<div class="row">
 			<div class="col-12 col-xl-9 mx-auto">
-				<div class="blog-title">
-					<h2 class="title">From Our Blog </h2>
-					<p class="text">The latest news, videos, and discussion topics</p>
-				</div>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-12 col-xl-9 mx-auto">
 				<div class="single-blog text-left">
 					<?php echo $html_thumbnail; ?>
 					<div class="blog-post-content pt-30">
 						<h5 class="sub-title font-style-normal">
-							Posted by <a class="blog-link" href="https://themeforest.net/user/hastech">HasTech</a> <span class="separator">/</span>
-							<?php echo $date; ?> <span class="separator">/</span> <a class="blog-link" href="https://themeforest.net/user/hastech">Electronics</a>
+							Posted by <?php echo $post_author; ?> <span class="separator">/</span>
+							<?php echo $post_date; ?> <span class="separator">/</span> <?php echo $post_categories; ?>
 						</h5>
-						<?php echo $content; ?>
+						<?php echo $post_content; ?>
 					</div>
 				</div>
 			</div>
