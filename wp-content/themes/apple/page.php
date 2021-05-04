4e805cf47feff6149ae9b9784d34c445f440c85a
Max Kardanets <max.kardanets@gmail.com>
2021-05-04 16:41:44 +0300

    ADDED | Default page

--- wp-content/themes/apple/page.php
+++ wp-content/themes/apple/page.php
@@ -1,1 +1,26 @@
-page.php
+<?php
+$title = get_the_title();
+$html_thumbnail = get_the_post_thumbnail();
+$content = get_the_content();
+
+get_header();
+
+echo get_theme_page_title_block($title);
+?>
+<!-- product tab start -->
+<section class="blog-section pt-80 pb-80">
+	<div class="container">
+		<div class="row">
+			<div class="col-12 col-xl-9 mx-auto">
+				<div class="single-blog text-left">
+					<?php echo $html_thumbnail; ?>
+					<div class="blog-post-content pt-30">
+						<?php echo $content; ?>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- product tab end -->
+<?php get_footer(); ?>
